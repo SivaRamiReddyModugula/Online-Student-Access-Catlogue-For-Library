3acc7393a8fec878f74d9817a7e2afe74878758b
Siva Rami Reddy <you@example.com>
2020-08-03 21:23:25 +0530

    updated logincheck

--- src/main/java/DBConnection/DBConnection.java
+++ src/main/java/DBConnection/DBConnection.java
@@ -12,10 +12,13 @@ public class DBConnection {
     public static Connection getConnection(){
         try {
             DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-            String url="jdbc:mysql://localhost:3306/library";
-            String username="root";
-            String password="root";
-            con = DriverManager.getConnection(url,username,password);
+			
+			 String url="jdbc:mysql://localhost:3306/library"; 
+			 String username="root";
+			 String password="root";
+			 con = DriverManager.getConnection(url,username,password);
+			 
+//            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","root");
             if(con!=null){
             return con;
             }else{
